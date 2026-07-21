349706c300d3ff8e492a9ee7cbbf42bbe5741071
toocolors <noa.algorithms@gmail.com>
2026-07-21 15:14:51 -0700

    Add edit assignment button.
    Add button to edit assignments. Edit button shows a modal that allows the user to enter a new title and/or due date for the assignment.

--- src/components/instructor/AssignmentUpdate.jsx
+++ src/components/instructor/AssignmentUpdate.jsx
@@ -9,26 +9,62 @@ const AssignmentUpdate = ({ editAssignment, onClose }) => {
   const [assignment, setAssignment] = useState({});
   const dialogRef = useRef();
 
+  const onChange = (event) => {
+    setAssignment({ ...assignment, [event.target.name]: event.target.value });
+  };
+
   /*
    *  dialog for edit of an assignment
    */
   const editOpen = () => {
     setMessage('');
     setAssignment(editAssignment);
-    // to be implemented.  invoke showModal() method on the dialog element.
-    // dialogRef.current.showModal();
+    dialogRef.current.showModal();
   };
 
+  // Closes edit assignment dialog and refreshes assignment list
+  const editClose = () => {
+    dialogRef.current.close();
+    onClose();
+  }
 
+  // Send put request to update assignment in database
+  const onSave = async () => {
+    try {
+      // Send put request
+      const response = await fetch(`${GRADEBOOK_URL}/assignments`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type':'application/json',
+          'Authorization': sessionStorage.getItem("jwt"),
+        },
+        body: JSON.stringify(assignment)
+      });
+
+      // Check response
+      if (response.ok) {
+        const data = await response.json();
+        alert(`Assignment ${data.id} successfully updated.`)
+        setMessage('');
+      } else {
+        const data = await response.json();
+        setMessage(data);
+      }
+    } catch (err) {
+      alert("request failed " + err);
+    }
+  }
 
   return (
     <>
       <button onClick={editOpen}>Edit</button>
-      <dialog ref={dialogRef} >
-        <p>To be implemented.  Show the id, title and due date of the assignemnt.
-          Allow user to edit the title and due date.
-          Buttons for Close and Save.
-        </p>
+      <dialog ref={dialogRef}>
+        <h2>Edit Assignment {assignment.id}</h2>
+        <Messages response={message} />
+        <input id="title" type="text" name="title" placeholder="Title" onChange={onChange} value={assignment.title}/>
+        <input id="dueDate" type="text" name="dueDate" placeholder="Due Date" onChange={onChange} value={assignment.dueDate}/>
+        <button id="saveButton" onClick={onSave}>save</button>
+        <button id="closeButton" onClick={editClose}>close</button>
       </dialog>
     </>
   )
